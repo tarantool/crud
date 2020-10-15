8d3a35f335bcb8b570ee50bdd6722c0c9003137a
Sergey Volgin <savolgin@gmail.com>
2020-10-15 13:30:24 +0400

    Add indexes with same names as fields

--- test/entrypoint/srv_select.lua
+++ test/entrypoint/srv_select.lua
@@ -24,6 +24,7 @@ package.preload['customers-storage'] = function()
                 if_not_exists = true,
                 engine = engine,
             })
+            --primary index
             customers_space:create_index('id_index', {
                 parts = { {field = 'id'} },
                 if_not_exists = true,
@@ -36,6 +37,16 @@ package.preload['customers-storage'] = function()
             customers_space:create_index('age_index', {
                 parts = { {field = 'age'} },
                 unique = false,
+                if_not_exists = true,
+            })
+            --indexes with same names as fields
+            customers_space:create_index('age', {
+                parts = { {field = 'age'} },
+                unique = false,
+                if_not_exists = true,
+            })
+            customers_space:create_index('id', {
+                parts = { {field = 'id'} },
                 if_not_exists = true,
             })
             customers_space:create_index('full_name', {
