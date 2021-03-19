b8ad716246ffd932451e677ea4307aa8ad9d365d
Oleg Babin <babinoleg@mail.ru>
2021-03-19 17:42:39 +0300

    test

--- deps.sh
+++ deps.sh
@@ -2,7 +2,7 @@
 # Call this script to install test dependencies
 
 # Test dependencies:
-tarantoolctl rocks install luatest 0.5.0
+tarantoolctl rocks install luatest 0.5.2
 tarantoolctl rocks install luacheck 0.25.0
 
-tarantoolctl rocks install cartridge 2.3.0
+tarantoolctl rocks install cartridge 2.5.0
