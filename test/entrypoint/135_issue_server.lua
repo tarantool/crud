361dd226f17c1bd0e1d7c98480d98cd7e29f78e4
Michael Filonenko <filonenko.mikhail@gmail.com>
2021-04-03 02:01:07 +0300

    Add test for 135 issue, simple select from two routers

---
+++ test/entrypoint/135_issue_server.lua
@@ -0,0 +1,71 @@
+#!/usr/bin/env tarantool
+
+require('strict').on()
+_G.is_initialized = function() return false end
+
+local log = require('log')
+local errors = require('errors')
+local cartridge = require('cartridge')
+local crud_utils = require('crud.common.utils')
+
+package.preload['customers-storage'] = function()
+    local engine = os.getenv('ENGINE') or 'memtx'
+    return {
+        role_name = 'customers-storage',
+        init = function(opts)
+            if not opts.is_master then
+                return
+            end
+            -- authTemplates
+            local authTemplates_space = box.schema.space.create('authTemplates', {
+                engine = 'memtx',
+                if_not_exists = true,
+            })
+            log.info('authTemplates space was configured')
+
+            authTemplates_space:format({
+                {name='msisdn',type='string'},
+                {name='channel',type='string'},
+                {name='password',type='string'},
+                {name='counter',type='unsigned'},
+                {name='create_date',type='unsigned'},
+                {name='ttl',type='unsigned'},
+                {name='bucket_id', type='unsigned'},
+            })
+            log.info('authTemplates was formatted')
+
+            authTemplates_space:create_index('authTemplates_msisdn_channel_idx', {parts={{field='msisdn'}, {field='channel'}},
+                type = 'TREE',
+                if_not_exists=true})
+            log.info('authTemplates_msisdn_channel_idx')
+
+            authTemplates_space:create_index('bucket_id', {parts={{field='bucket_id'}},
+                unique=false,
+                if_not_exists=true})
+            log.info('authTemplates_bucket_id')
+
+            authTemplates_space:create_index('ttl', {parts={{field='ttl'}},
+                unique=false,
+                if_not_exists=true})
+            log.info('authTemplates_ttl')
+        end,
+    }
+end
+
+local ok, err = errors.pcall('CartridgeCfgError', cartridge.cfg, {
+    advertise_uri = 'localhost:3301',
+    http_port = 8081,
+    bucket_count = 3000,
+    roles = {
+        'cartridge.roles.crud-router',
+        'cartridge.roles.crud-storage',
+        'customers-storage',
+    },
+})
+
+if not ok then
+    log.error('%s', err)
+    os.exit(1)
+end
+
+_G.is_initialized = cartridge.is_healthy
---
+++ test/integration/135_issue_test.lua
@@ -0,0 +1,93 @@
+local fio = require('fio')
+
+local t = require('luatest')
+local crud_utils = require('crud.common.utils')
+
+local helpers = require('test.helper')
+
+local pgroup = helpers.pgroup.new('135_issue_test', {
+    engine = {'memtx', 'vinyl'},
+})
+
+pgroup:set_before_all(function(g)
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = helpers.entrypoint('135_issue_server'),
+        use_vshard = true,
+        replicasets = {
+            {
+                uuid = helpers.uuid('a'),
+                alias = 'router',
+                roles = { 'crud-router' },
+                servers = {
+                    { instance_uuid = helpers.uuid('a', 1), alias = 'router' },
+                },
+            },
+            {
+                uuid = helpers.uuid('b'),
+                alias = 's-1',
+                roles = { 'customers-storage', 'crud-storage' },
+                servers = {
+                    { instance_uuid = helpers.uuid('b', 1), alias = 's1-master' },
+                    { instance_uuid = helpers.uuid('b', 2), alias = 's1-replica' },
+                },
+            },
+            {
+                uuid = helpers.uuid('c'),
+                alias = 's-2',
+                roles = { 'customers-storage', 'crud-storage' },
+                servers = {
+                    { instance_uuid = helpers.uuid('c', 1), alias = 's2-master' },
+                    { instance_uuid = helpers.uuid('c', 2), alias = 's2-replica' },
+                },
+            },
+            {
+                uuid = helpers.uuid('d'),
+                alias = 'router2',
+                roles = { 'crud-router' },
+                servers = {
+                    { instance_uuid = helpers.uuid('d', 1), alias = 'router2' },
+                },
+            },
+        },
+        env = {
+            ['ENGINE'] = g.params.engine,
+        },
+    })
+
+    g.cluster:start()
+end)
+
+pgroup:set_after_all(function(g) helpers.stop_cluster(g.cluster) end)
+
+
+pgroup:add('test_auth_templates', function(g)
+    local templates = g.cluster.main_server.net_box:call('crud.insert', {'authTemplates', {"79774120882","app4t2","671418",3,1716591611,1716592211}})
+    t.assert_not_equals(templates, nil)
+
+    local obj, err = g.cluster.main_server.net_box:call(
+       'crud.select', {'authTemplates', {{'==', 'authTemplates_msisdn_channel_idx', {'79774120882', 'app4t2'}}}}
+    )
+    t.assert_not_equals(obj, nil, err)
+    t.assert_equals(err, nil, err)
+
+    local obj, err = g.cluster.main_server.net_box:call(
+       'crud.select', {'authTemplates', {{'==', 'authTemplates_msisdn_channel_idx', {'79774120882'}}}}
+    )
+    t.assert_not_equals(obj, nil)
+    t.assert_equals(err, nil, err)
+
+
+    local obj, err = g.cluster:server('router2').net_box:call(
+       'crud.select', {'authTemplates', {{'==', 'authTemplates_msisdn_channel_idx', {'79774120882', 'app4t2'}}}}
+    )
+    t.assert_not_equals(obj, nil, err)
+    t.assert_equals(err, nil, err)
+
+    local obj, err = g.cluster:server('router2').net_box:call(
+       'crud.select', {'authTemplates', {{'==', 'authTemplates_msisdn_channel_idx', {'79774120882'}}}}
+    )
+    t.assert_not_equals(obj, nil)
+    t.assert_equals(err, nil, err)
+
+end)
