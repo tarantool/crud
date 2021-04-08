2477c785a7afd147d7fb8c012a13559aae5a07a8
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-04-08 19:49:38 +0300

    Add hot-reload role test

--- deps.sh
+++ deps.sh
@@ -7,5 +7,5 @@ set -e
 tarantoolctl rocks install luatest 0.5.0
 tarantoolctl rocks install luacheck 0.25.0
 
-tarantoolctl rocks install cartridge 2.3.0
+tarantoolctl rocks install cartridge 2.5.1
 tarantoolctl rocks make
