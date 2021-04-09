0a019f1acd4312b422ddd4fccdc903a5d389a397
AnaNek <neknas28@gmail.com>
2021-04-09 13:29:11 +0300

    Add test for cut_rows with pairs result

--- test/integration/pairs_test.lua
+++ test/integration/pairs_test.lua
@@ -543,3 +543,49 @@ pgroup:add('test_pairs_partial_result', function(g)
     ]], {conditions, fields})
     t.assert_equals(objects, expected_customers)
 end)
+
+pgroup:add('test_pairs_cut_rows', function(g)
+    helpers.insert_objects(g, 'customers', {
+        {
+            id = 1, name = "Elizabeth", last_name = "Jackson",
+            age = 12, city = "Los Angeles",
+        }, {
+            id = 2, name = "Mary", last_name = "Brown",
+            age = 46, city = "London",
+        }, {
+            id = 3, name = "David", last_name = "Smith",
+            age = 33, city = "Los Angeles",
+        }, {
+            id = 4, name = "William", last_name = "White",
+            age = 46, city = "Chicago",
+        },
+    })
+
+    -- condition by indexed non-unique non-primary field (age):
+    local conditions = {{'>=', 'age', 33}}
+
+    -- condition field is not in opts.fields
+    local fields = {'name', 'city'}
+
+    -- result doesn't contain primary key, result tuples are sorted by field+primary
+    -- in age + id order
+    local expected_customers = {
+        {name = "David", city = "Los Angeles"},
+        {name = "Mary", city = "London"},
+        {name = "William", city = "Chicago"},
+    }
+
+    local objects = g.cluster.main_server.net_box:eval([[
+        local crud = require('crud')
+
+        local conditions, fields = ...
+
+        local objects = {}
+        for _, object in crud.pairs('customers', conditions, {use_tomap = true, fields = fields}) do
+            table.insert(objects, object)
+        end
+
+        return crud.cut_rows(objects, nil, fields, {mapped = true})
+    ]], {conditions, fields})
+    t.assert_equals(objects.rows, expected_customers)
+end)
