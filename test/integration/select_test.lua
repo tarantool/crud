c46284e98951c928db322f59c893520209f52a95
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-06-30 17:39:09 +0300

    Added a test containing several filters, one of which is by partial index

--- test/integration/select_test.lua
+++ test/integration/select_test.lua
@@ -915,6 +915,13 @@ pgroup:add('test_multipart_primary_index', function(g)
     local objects = crud.unflatten_rows(result.rows, result.metadata)
     t.assert_equals(objects, helpers.get_objects_by_idxs(coords, {1, 2}))
 
+    local new_conditions = {{'=', 'y', 1}, {'=', 'primary', 0}}
+    local result, err = g.cluster.main_server.net_box:call('crud.select', {'coord', new_conditions,
+                                                                            {after = result_0.rows[3], first = -2}})
+    t.assert_equals(err, nil)
+    local objects = crud.unflatten_rows(result.rows, result.metadata)
+    t.assert_equals(objects, helpers.get_objects_by_idxs(coords, {2}))
+
     local conditions = {{'=', 'primary', {0, 2}}}
     local result, err = g.cluster.main_server.net_box:call('crud.select', {'coord', conditions})
     t.assert_equals(err, nil)
