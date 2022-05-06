50d59e76bbdf89bff959271ba40f1fd9ff029ece
Alexander Turenko <alexander.turenko@tarantool.org>
2022-05-06 03:54:58 +0300

    doc: add a script to ease experimenting with crud
    Basic usage:
    
    ```shell
    $ ./doc/playground.lua
    tarantool> crud.select('customers', {{'<=', 'age', 35}})
    tarantool> crud.select('developers', nil, {first = 6})
    tarantool> crud.<something else>(<...>)
    ```
    
    What the script is doing, step by step:
    
    * Configure the database to don't leave WAL and snapshot files.
    * Configure and bootstrap vshard storage and router on the single
      instance.
    * Create `customers` and `developers` spaces and fill them with data.
    * Start a console.

---
+++ doc/playground.lua
@@ -0,0 +1,150 @@
+#!/usr/bin/env tarantool
+
+-- How to run:
+--
+-- $ ./doc/playground.lua
+--
+-- Or
+--
+-- $ KEEP_DATA=1 ./doc/playground.lua
+--
+-- What to do next:
+--
+-- Choose an example from README.md, doc/select.md or doc/pairs.md
+-- and run. For example:
+--
+-- tarantool> crud.select('customers', {{'<=', 'age', 35}}, {first = 10})
+-- tarantool> crud.select('developers', nil, {first = 6})
+
+local fio = require('fio')
+local console = require('console')
+local vshard = require('vshard')
+local crud = require('crud')
+
+-- Trick to don't leave *.snap, *.xlog files. See
+-- test/tuple_keydef.test.lua in the tuple-keydef module.
+if os.getenv('KEEP_DATA') ~= nil then
+    box.cfg()
+else
+    local tempdir = fio.tempdir()
+    box.cfg({
+        memtx_dir = tempdir,
+        wal_mode = 'none',
+    })
+    fio.rmtree(tempdir)
+end
+
+-- Setup vshard.
+_G.vshard = vshard
+box.once('guest', function()
+    box.schema.user.grant('guest', 'super')
+end)
+local uri = 'guest@localhost:3301'
+local cfg = {
+    bucket_count = 3000,
+    sharding = {
+        [box.info().cluster.uuid] = {
+            replicas = {
+                [box.info().uuid] = {
+                    uri = uri,
+                    name = 'storage',
+                    master = true,
+                },
+            },
+        },
+    },
+}
+vshard.storage.cfg(cfg, box.info().uuid)
+vshard.router.cfg(cfg)
+vshard.router.bootstrap()
+
+-- Create the 'customers' space.
+box.once('customers', function()
+    box.schema.create_space('customers', {
+        format = {
+            {name = 'id', type = 'unsigned'},
+            {name = 'bucket_id', type = 'unsigned'},
+            {name = 'name', type = 'string'},
+            {name = 'age', type = 'number'},
+        }
+    })
+    box.space.customers:create_index('primary_index', {
+        parts = {
+            {field = 1, type = 'unsigned'},
+        },
+    })
+    box.space.customers:create_index('bucket_id', {
+        parts = {
+            {field = 2, type = 'unsigned'},
+        },
+        unique = false,
+    })
+    box.space.customers:create_index('age', {
+        parts = {
+            {field = 4, type = 'number'},
+        },
+        unique = false,
+    })
+
+    -- Fill the space.
+    box.space.customers:insert({1, 477, 'Elizabeth', 12})
+    box.space.customers:insert({2, 401, 'Mary', 46})
+    box.space.customers:insert({3, 2804, 'David', 33})
+    box.space.customers:insert({4, 1161, 'William', 81})
+    box.space.customers:insert({5, 1172, 'Jack', 35})
+    box.space.customers:insert({6, 1064, 'William', 25})
+    box.space.customers:insert({7, 693, 'Elizabeth', 18})
+end)
+
+-- Create the developers space.
+box.once('developers', function()
+    box.schema.create_space('developers', {
+        format = {
+            {name = 'id', type = 'unsigned'},
+            {name = 'bucket_id', type = 'unsigned'},
+            {name = 'name', type = 'string'},
+            {name = 'surname', type = 'string'},
+            {name = 'age', type = 'number'},
+        }
+    })
+    box.space.developers:create_index('primary_index', {
+        parts = {
+            {field = 1, type = 'unsigned'},
+        },
+    })
+    box.space.developers:create_index('bucket_id', {
+        parts = {
+            {field = 2, type = 'unsigned'},
+        },
+        unique = false,
+    })
+    box.space.developers:create_index('age_index', {
+        parts = {
+            {field = 5, type = 'number'},
+        },
+        unique = false,
+    })
+    box.space.developers:create_index('full_name', {
+        parts = {
+            {field = 3, type = 'string'},
+            {field = 4, type = 'string'},
+        },
+        unique = false,
+    })
+
+    -- Fill the space.
+    box.space.developers:insert({1, 477, 'Alexey', 'Adams', 20})
+    box.space.developers:insert({2, 401, 'Sergey', 'Allred', 21})
+    box.space.developers:insert({3, 2804, 'Pavel', 'Adams', 27})
+    box.space.developers:insert({4, 1161, 'Mikhail', 'Liston', 51})
+    box.space.developers:insert({5, 1172, 'Dmitry', 'Jacobi', 16})
+    box.space.developers:insert({6, 1064, 'Alexey', 'Sidorov', 31})
+end)
+
+-- Initialize crud.
+crud.init_storage()
+crud.init_router()
+
+-- Start a console.
+console.start()
+os.exit()
