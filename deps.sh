195515c0a9ac1ad2605f28e8ef5e9a03a299706e
Alex Vakhov <alex.vakhov@gmail.com>
2025-09-29 10:35:46 +0500

    test: remove skip for vshard config support

--- deps.sh
+++ deps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Call this script to install test dependencies
+# Usage examples:
+#   CARTRIDGE_VERSION=2.16.3 ./deps.sh
+#   VSHARD_VERSION=0.1.36 ./deps.sh
 
 set -e
 
@@ -13,37 +16,21 @@ fi
 # Test dependencies:
 ${TTCTL} rocks install luatest 1.0.1
 ${TTCTL} rocks install luacheck 0.26.0
-${TTCTL} rocks install luacov 0.13.0
-${TTCTL} rocks install luacov-reporters 0.1.0
 
-# cluacov, luacov-coveralls and dependencies
-$TTCTL rocks install https://raw.githubusercontent.com/luarocks/cluacov/master/cluacov-dev-1.rockspec
-$TTCTL rocks install https://luarocks.org/manifests/dhkolf/dkjson-2.8-1.rockspec
-$TTCTL rocks install https://raw.githubusercontent.com/keplerproject/luafilesystem/master/luafilesystem-scm-1.rockspec
-$TTCTL rocks install https://raw.githubusercontent.com/moteus/lua-path/master/rockspecs/lua-path-scm-0.rockspec
+if [[ -n "${CARTRIDGE_VERSION}" ]]
+then
+    ${TTCTL} rocks install https://raw.githubusercontent.com/luarocks/cluacov/master/cluacov-dev-1.rockspec
+    ${TTCTL} rocks install https://luarocks.org/manifests/dhkolf/dkjson-2.8-1.rockspec
+    ${TTCTL} rocks install https://raw.githubusercontent.com/keplerproject/luafilesystem/master/luafilesystem-scm-1.rockspec
+    ${TTCTL} rocks install https://raw.githubusercontent.com/moteus/lua-path/master/rockspecs/lua-path-scm-0.rockspec
+    ${TTCTL} rocks install luacov-coveralls --only-server=https://luarocks.org/
 
-# Most of this code is the workaround for
-# https://github.com/moteus/luacov-coveralls/pull/30
-# Remove it, when the pull request will be merged.
-TMPDIR="$(mktemp -d)"
-LUACOV_COVERALLS_ROCKSPEC_URL="https://raw.githubusercontent.com/moteus/luacov-coveralls/master/rockspecs/luacov-coveralls-scm-0.rockspec"
-LUACOV_COVERALLS_ROCKSPEC_FILE="${TMPDIR}/luacov-coveralls-scm-0.rockspec"
-curl -fsSL "${LUACOV_COVERALLS_ROCKSPEC_URL}" > "${LUACOV_COVERALLS_ROCKSPEC_FILE}"
-sed -i -e 's@git://@git+https://@' "${LUACOV_COVERALLS_ROCKSPEC_FILE}"
-$TTCTL rocks install "${LUACOV_COVERALLS_ROCKSPEC_FILE}"
-rm "${LUACOV_COVERALLS_ROCKSPEC_FILE}"
-rmdir "${TMPDIR}"
+    ${TTCTL} rocks install cartridge "${CARTRIDGE_VERSION}"
+    ${TTCTL} rocks install migrations 1.1.0
+else
+    ${TTCTL} rocks install vshard "${VSHARD_VERSION:-0.1.36}"
+fi
 
-CARTRIDGE_VERSION="${CARTRIDGE_VERSION:-2.16.3}"
-VSHARD_VERSION="${VSHARD_VERSION:-0.1.36}"
-METRICS_VERSION="${METRICS_VERSION:-1.5.0}"
-DDL_VERSION="${METRICS_VERSION:-1.7.1}"
-MIGRATIONS_VERSION="${MIGRATIONS_VERSION:-1.1.0}"
-
-${TTCTL} rocks install cartridge "${CARTRIDGE_VERSION}"
-${TTCTL} rocks install vshard "${VSHARD_VERSION}"
-${TTCTL} rocks install metrics "${METRICS_VERSION}"
-${TTCTL} rocks install ddl "${DDL_VERSION}"
-${TTCTL} rocks install migrations "${MIGRATIONS_VERSION}"
+${TTCTL} rocks install ddl 1.7.1
 
 ${TTCTL} rocks make
