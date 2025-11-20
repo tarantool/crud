152da920cb1becb5df8921cedcc4526d0d532122
Sergey Morozov <sergey.morozov@vkteam.ru>
2025-11-20 20:33:24 +0300

    TNTP-2109: Add rebalance related metrics

--- crud/common/rebalance.lua
+++ crud/common/rebalance.lua
@@ -3,9 +3,10 @@ local log = require('log')
 local vshard_consts = require('vshard.consts')
 local utils = require('crud.common.utils')
 
+local has_metrics_module, metrics = pcall(require, 'metrics')
+
 local SETTINGS_SPACE_NAME = '_crud_settings'
 local SAFE_MOD_ENABLE_EVENT = '_crud.safe_mode_enable'
-
 
 local M = {
     safe_mode = false,
@@ -85,6 +86,8 @@ local function safe_mode_disable()
 end
 
 local function rebalance_init()
+    M.metrics.enable_storage_metrics()
+
     -- box.watch was introduced in tarantool 2.10.0
     if not utils.tarantool_supports_box_watch() then
         log.warn('This version of tarantool does not support autoswitch to safe mode during rebalance. '
@@ -131,18 +134,52 @@ local function rebalance_stop()
 end
 
 local function router_cache_clear()
-    local r = utils.get_vshard_router_instance()
     M._router_cache_last_clear_ts = fiber.time()
-    return r:_route_map_clear()
+    return utils.get_vshard_router_instance():_route_map_clear()
 end
 
 local function router_cache_length()
-    local r = utils.get_vshard_router_instance()
-    return r.known_bucket_count
+    return utils.get_vshard_router_instance().known_bucket_count
 end
 
 local function router_cache_last_clear_ts()
     return M._router_cache_last_clear_ts
+end
+
+-- Rebalance related metrics
+local function enable_storage_metrics()
+    if not has_metrics_module then
+        return
+    end
+
+    local safe_mode_enabled_gauge = metrics.gauge(
+            'tnt_crud_storage_safe_mode_enabled',
+            "is safe mode enabled on this storage instance"
+    )
+
+    metrics.register_callback(function()
+        safe_mode_enabled_gauge:set(safe_mode_status() and 1 or 0)
+    end)
+end
+
+local function enable_router_metrics()
+    if not has_metrics_module then
+        return
+    end
+
+    local router_cache_length_gauge = metrics.gauge(
+            'tnt_crud_router_cache_length',
+            "number of bucket routes in vshard router cache"
+    )
+    local router_cache_last_clear_ts_gauge = metrics.gauge(
+            'tnt_crud_router_cache_last_clear_ts',
+            "when vshard router cache was cleared last time"
+    )
+
+    metrics.register_callback(function()
+        router_cache_length_gauge:set(router_cache_length())
+        router_cache_last_clear_ts_gauge:set(router_cache_last_clear_ts())
+    end)
 end
 
 M.init = rebalance_init
@@ -165,6 +202,11 @@ M.storage_api = {
     rebalance_safe_mode_status = safe_mode_status,
     rebalance_safe_mode_enable = safe_mode_enable,
     rebalance_safe_mode_disable = safe_mode_disable,
+}
+
+M.metrics = {
+    enable_storage_metrics = enable_storage_metrics,
+    enable_router_metrics = enable_router_metrics,
 }
 
 return M
