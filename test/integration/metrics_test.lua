f64f7865b096980e4996de800dfdf73405882557
Sergey Morozov <sergey.morozov@vkteam.ru>
2026-02-02 20:33:33 +0300

    safe/fast mode: add `tnt_crud_router_cache_clear_ts` metric to router
    This metric can help user to monitor routers status
    in the cluster when switching it back to fast mode.
    
    Also changed metrics callbacks to prevent duplicate metrics
    if init function is called multiple times.

--- test/integration/metrics_test.lua
+++ test/integration/metrics_test.lua
@@ -1,3 +1,4 @@
+local fiber = require('fiber')
 local helpers = require('test.helper')
 local t = require('luatest')
 
@@ -80,6 +81,63 @@ pgroup.test_safe_mode_storage_metrics = function(g)
         end
         if not has_metric then
             t.fail('No tnt_crud_storage_safe_mode_enabled metric found on storage')
+        end
+    end)
+end
+
+pgroup.test_router_cache_metrics = function(g)
+    local has_metrics_module = require('metrics')
+    t.skip_if(not has_metrics_module, 'No metrics module in current version')
+
+    -- Check router cache metric initial value on router
+    local observed = g.router:eval("return require('metrics').collect({ invoke_callbacks = true })")
+    local has_metric = false
+    for _, m in pairs(observed) do
+        if m.metric_name == 'tnt_crud_router_cache_clear_ts' then
+            t.assert_equals(m.value, 0, 'Cache never cleared')
+            has_metric = true
+            break
+        end
+    end
+    if not has_metric then
+        t.fail('No tnt_crud_router_cache_clear_ts metric found on router')
+    end
+
+    -- Check no router cache metric on storage
+    helpers.call_on_storages(g.cluster, function(server)
+        observed = server:eval("return require('metrics').collect({ invoke_callbacks = true })")
+        for _, m in pairs(observed) do
+            if m.metric_name == 'tnt_crud_router_cache_clear_ts' then
+                t.fail('tnt_crud_router_cache_clear_ts metric found on storage')
+            end
+        end
+    end)
+
+    -- Clear router cache
+    local expected_ts = fiber.time()
+    g.router:call("crud.rebalance.router_cache_clear")
+
+    -- Check router cache metric new value on router
+    observed = g.router:eval("return require('metrics').collect({ invoke_callbacks = true })")
+    has_metric = false
+    for _, m in pairs(observed) do
+        if m.metric_name == 'tnt_crud_router_cache_clear_ts' then
+            t.assert_almost_equals(m.value, expected_ts, 5, 'Cache never cleared')
+            has_metric = true
+            break
+        end
+    end
+    if not has_metric then
+        t.fail('No tnt_crud_router_cache_clear_ts metric found on router')
+    end
+
+    -- Check no router cache metric appeared on storage
+    helpers.call_on_storages(g.cluster, function(server)
+        observed = server:eval("return require('metrics').collect({ invoke_callbacks = true })")
+        for _, m in pairs(observed) do
+            if m.metric_name == 'tnt_crud_router_cache_clear_ts' then
+                t.fail('tnt_crud_router_cache_clear_ts metric found on storage')
+            end
         end
     end)
 end
